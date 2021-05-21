0712da1d267313278bb9ec9d5249c06a918143ea
megan <megan.schertz@gmail.com>
2021-05-21 15:50:10 -0500

    commit tree d050842190b8a65fe1222dc9431dec8eebf4bcfd

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(array, funct) {
+    const newArray = []
+    for(let i = 0; i < array.length; i++){
+        newArray.push(funct(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, funct, startingValue) {
+    let total = (!!startingValue) ? startingValue : array[0]
+    let i = (!!startingValue) ? 0 : 1
+    for (i; i < array.length; i++) {
+      total = funct(array[i], total)
+    }
+    return total
+}
